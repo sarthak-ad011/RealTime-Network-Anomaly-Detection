288af05e4979dce910235e2056fdebfa6ff087fb
Sarthak <sarthakanand1409@gmail.com>
2026-07-03 15:54:54 +0530

    feat: add terraform modules for ECR repository provisioning and GitHub OIDC role configuration

---
+++ terraform/modules/github_oidc/main.tf
@@ -0,0 +1,54 @@
+variable "github_repo" { type = string }
+variable "ecr_repository_arn" { type = string }
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-anomaly"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+      Action    = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = { "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com" }
+        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*" }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name = "ecr-push"
+  role = aws_iam_role.github_actions.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability", "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart", "ecr:CompleteLayerUpload", "ecr:PutImage",
+          "ecr:BatchGetImage", "ecr:GetDownloadUrlForLayer"
+        ]
+        Resource = var.ecr_repository_arn
+      }
+    ]
+  })
+}
+
+output "role_arn" { value = aws_iam_role.github_actions.arn }
